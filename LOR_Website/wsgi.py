9d5e2334e97d4945b5df19d83f34a9bb6cdaba45
Toxicfireball <txzspartan117@gmail.com>
2025-05-24 02:32:37 +0800

    static fixing3

--- LOR_Website/wsgi.py
+++ LOR_Website/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LOR_Website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lor_Website.settings')
 
 application = get_wsgi_application()
