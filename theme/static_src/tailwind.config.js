f95edd782bba26df4ffa9dd1fab9a3ead07bd673
Toxicfireball <txzspartan117@gmail.com>
2025-06-15 06:05:00 +0800

    Removed bootstrap

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,16 +1,27 @@
+// tailwind.config.js
 module.exports = {
-  corePlugins: {
-    preflight: false,
-  },
   content: [
     './templates/**/*.html',
-    // …
+    './theme/templates/**/*.html',
+    './*/templates/**/*.html',
   ],
+  // put back preflight so you get sensible base styles
+  corePlugins: {
+    preflight: true,
+  },
   safelist: [
-    'collapse', 'collapsing', 'show',
-    'navbar-collapse', 'navbar-nav',
-    'dropdown', 'dropdown-menu', 'dropdown-menu-dark',
-    'navbar-toggler', 'navbar-toggler-icon'
+    // If you still need to guarantee some classes, you can list them here.
+    // But in most cases, just correctly pointing at your HTML is enough.
+    'bg-gray-800',
+    'text-white',
+    'flex', 'items-center', 'justify-between', 'hidden', 'block', 
+    'md:flex', 'md:hidden', 'max-w-7xl', 'mx-auto',
+    'px-4','sm:px-6','lg:px-8','h-16','p-2','rounded',
+    'hover:bg-gray-700','focus:outline-none','h-6','w-6','fill-current',
+    'border-t','border-gray-600','mt-2','relative','absolute','pl-4'
   ],
-  // …
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
